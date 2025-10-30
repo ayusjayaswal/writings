0fcf6f8c6fc89765ec16509d5a5cb8cd46c91824
Ayush Jayaswal <ayushjayaswal@yandex.com>
2025-10-31 00:17:24 +0530

    drafter

---
+++ drafter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Draft a new manuscript :D"
+echo -n "filename => "
+read file
+[[ "$file" != *.md ]] && file="${file}.md"
+touch "$file"
+echo -n "title= "
+read title
+date=$(date +%Y-%M-%d)
+
+{
+	echo "+++"
+	echo "title = \"$title\""
+	echo "date = \"$date\""
+	echo "[taxonomies]"
+	echo "tags = []"
+	echo "+++"
+} >> "$file"
+
+echo "DONE :O"
